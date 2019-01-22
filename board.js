bd707f9ed2df4e7d4b4c6c49f53a885bd6369901
emeth- <seanybob@gmail.com>
2019-01-22 13:39:10 -0800

    more

--- board.js
+++ board.js
@@ -13,8 +13,8 @@ function start_mission() {
     set_up_enemies();
     set_up_mountains();
     set_up_villages();
-    //board = set_up_soldiers(board, my_team);
-    //board = set_up_items(board, mission, battle);
+    set_up_soldiers();
+    set_up_items();
 }
 
 function set_up_treasure() {
@@ -189,6 +189,118 @@ function set_up_villages() {
     window.board[7][rand(0,4)+5] = vlg;
     window.board[8][rand(0,4)+5] = vlg;
 }
+
+
+function set_up_soldiers() {
+    for(var i=0; i<5; i++) {
+        var soldier = window.my_team[i];
+        window.board[9][i*2] = {
+            "type": "team",
+            "image": soldier[i]['image']
+        };
+    }
+}
+
+
+function set_up_items() {
+
+    potions_by_mission = {
+        1: "Health_Potion",
+        2: "Potion_of_Fortitude",
+        3: "Mega_Potion",
+        4: "Potion_of_Well-Being",
+        5: "Potion_of_Fortitude",
+        6: "Potion_of_Well-Being",
+        7: "Potion_of_Fortitude",
+        8: "Mega_Potion",
+        9: "Potion_of_Well-Being",
+        10: "Potion_of_Fortitude",
+    };
+
+    attack_items_by_mission = {
+        1: ["Mace", "Broadsword"],
+        2: ["Bow", "Hammer"],
+        3: ["Magic_Force_Spell", "Magic_Force_Spell"],
+        4: ["Magic_Force_Spell", "Bow"],
+        5: ["Berserker_Battleaxe", "Berserker_Battleaxe"],
+        6: ["Sword_of_Deflection", "Sword_of_Deflection"],
+        7: ["Double_Sword", "Halberd"],
+        8: ["Magic_Lightening_Spell", "Magic_Lightening_Spell"],
+        9: ["Double_Axe", "Double_Axe"],
+        10: ["Double_Axe", "Magic_Lightening_Spell"],
+    };
+
+    defense_items_by_mission = {
+        1: ["Magic_Staff_of_Thunder", "Magic_Staff_of_Thunder"],
+        2: ["Amulet_of_Teleportation", "Amulet_of_Teleportation"],
+        3: ["Helmet", "Shield"],
+        4: ["Plate_Armor", "Magic_Cloak_of_Invisibility"],
+        5: ["Counter_Enchantment_Helmet", "Counter_Enchantment_Helmet"],
+        6: ["Plate_Armor", "Plate_Armor"],
+        7: ["Chainmail", "Chainmail"],
+        8: ["Leather_Armor", "Leather_Armor"],
+        9: ["Amulet_of_Teleportation", "Chainmail"],
+        10: ["Plate_Armor", "Chainmail"],
+    };
+
+    //potions
+    window.board[5][4] = {
+        "type": "potion",
+        "image": "images/".potions_by_mission[mission].".jpg"
+    };
+    window.board[5][6] = {
+        "type": "potion",
+        "image": "images/".potions_by_mission[mission].".jpg"
+    };
+
+    empty_spaces_for_item_spawns = [];
+    for(i=4; i<9; i++) {
+        for(j=0; j<10; j++) {
+            if(!window.board[i][j]['type']) {
+                empty_spaces_for_item_spawns.push([i, j]);
+            }
+        }
+    }
+
+    //Returns a random key to an empty space
+    item_spawn_spaces = array_rand(empty_spaces_for_item_spawns, 4);
+
+    attack_item_key_one = rand(0,1);
+    attack_item_key_two = rand(0,1);
+    defense_item_key_one = rand(0,1);
+    defense_item_key_two = rand(0,1);
+
+    item_1_spawn = empty_spaces_for_item_spawns[item_spawn_spaces[0]]; //Coordinate point, e.g. [4,5]
+    item_2_spawn = empty_spaces_for_item_spawns[item_spawn_spaces[1]];
+    item_3_spawn = empty_spaces_for_item_spawns[item_spawn_spaces[2]];
+    item_4_spawn = empty_spaces_for_item_spawns[item_spawn_spaces[3]];
+
+    board[item_1_spawn[0]][item_1_spawn[1]] = {
+        "type": "item",
+        "image": "images/".attack_items_by_mission[mission][attack_item_key_one].".jpg"
+    };
+
+    board[item_2_spawn[0]][item_2_spawn[1]] = {
+        "type": "item",
+        "image": "images/".attack_items_by_mission[mission][attack_item_key_two].".jpg"
+    };
+
+    board[item_3_spawn[0]][item_3_spawn[1]] = {
+        "type": "item",
+        "image": "images/".defense_items_by_mission[mission][defense_item_key_one].".jpg"
+    };
+
+    board[item_4_spawn[0]][item_4_spawn[1]] = {
+        "type": "item",
+        "image": "images/".defense_items_by_mission[mission][defense_item_key_two].".jpg"
+    };
+}
+
+
+
+
+
+
 
 
 
